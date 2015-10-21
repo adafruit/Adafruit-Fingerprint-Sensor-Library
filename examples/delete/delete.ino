dda09c284b3466ee098fb7a927252a386c550a5b
ladyada <limor@ladyada.net>
2015-10-21 01:13:13 -0400

    fix up delete to match

--- examples/delete/delete.ino
+++ examples/delete/delete.ino
@@ -1,7 +1,7 @@
 /*************************************************** 
   This is an example sketch for our optical Fingerprint sensor
 
-  Designed specifically to work with the Adafruit BMP085 Breakout 
+  Designed specifically to work with the Adafruit Fingerprint sensor
   ----> http://www.adafruit.com/products/751
 
   These displays use TTL Serial to communicate, 2 pins are required to 
@@ -15,25 +15,19 @@
  ****************************************************/
 
 #include <Adafruit_Fingerprint.h>
-#if ARDUINO >= 100
- #include <SoftwareSerial.h>
-#else
- #include <NewSoftSerial.h>
-#endif
+#include <SoftwareSerial.h>
 
 uint8_t getFingerprintEnroll(uint8_t id);
 
-
+// Software serial for when you dont have a hardware serial port
 // pin #2 is IN from sensor (GREEN wire)
 // pin #3 is OUT from arduino  (WHITE wire)
-#if ARDUINO >= 100
+// On Leonardo/Micro/Yun, use pins 8 & 9. On Mega, just grab a hardware serialport 
 SoftwareSerial mySerial(2, 3);
-//#define mySerial Serial1
-#else
-NewSoftSerial mySerial(2, 3);
-#endif
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 
-  Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+// On Leonardo/Micro or others with hardware serial, use those! #0 is green wire, #1 is white
+//Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial1);
 
 void setup()  
 {
@@ -53,6 +47,9 @@ void setup()
 
 void loop()                     // run over and over again
 {
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(500);
+
   Serial.println("Type in the ID # you want delete...");
   uint8_t id = 0;
   while (true) {
