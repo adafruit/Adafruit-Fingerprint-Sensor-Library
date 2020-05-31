876b9524b4fc7cd6bc17778b27f32628e3daf95a
lady ada <limor@ladyada.net>
2020-05-31 17:20:31 -0400

    make example pick platforms better

--- examples/ledcontrol/ledcontrol.ino
+++ examples/ledcontrol/ledcontrol.ino
@@ -1,8 +1,5 @@
 /*************************************************** 
-  This is an example sketch for our optical Fingerprint sensor
-
-  Designed specifically to work with the Adafruit BMP085 Breakout 
-  ----> http://www.adafruit.com/products/751
+  This is an example sketch for our optical Fingerprint sensor with LED ring
 
   These displays use TTL Serial to communicate, 2 pins are required to 
   interface
@@ -17,15 +14,21 @@
 
 #include <Adafruit_Fingerprint.h>
 
-// On Leonardo/Micro or others with hardware serial, use those! #0 is green wire, #1 is white
-// uncomment this line:
-// #define mySerial Serial1
 
+
+#if defined(__AVR__) || defined(ESP8266)
 // For UNO and others without hardware serial, we must use software serial...
 // pin #2 is IN from sensor (GREEN wire)
 // pin #3 is OUT from arduino  (WHITE wire)
-// comment these two lines if using hardware serial
+// Set up the serial port to use softwareserial..
 SoftwareSerial mySerial(2, 3);
+
+#else
+// On Leonardo/M0/etc, others with hardware serial, use hardware serial!
+// #0 is green wire, #1 is white
+#define mySerial Serial1
+
+#endif
 
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 
