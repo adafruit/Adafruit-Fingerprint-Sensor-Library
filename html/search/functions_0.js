cffc9de4a88ea92927393b1342922db30897800b
Doxygen CI <travis@travis-ci.org>
2020-10-15 15:32:27 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['adafruit_5ffingerprint',['Adafruit_Fingerprint',['../class_adafruit___fingerprint.html#a134e3243442218444fc273a5b2656f84',1,'Adafruit_Fingerprint']]],
+  ['adafruit_5ffingerprint',['Adafruit_Fingerprint',['../class_adafruit___fingerprint.html#a134e3243442218444fc273a5b2656f84',1,'Adafruit_Fingerprint::Adafruit_Fingerprint(HardwareSerial *hs, uint32_t password=0x0)'],['../class_adafruit___fingerprint.html#a50e1ba8004c2a9151b57f6dfccb0d9bb',1,'Adafruit_Fingerprint::Adafruit_Fingerprint(Stream *serial, uint32_t password=0x0)']]],
   ['adafruit_5ffingerprint_5fpacket',['Adafruit_Fingerprint_Packet',['../struct_adafruit___fingerprint___packet.html#a579e2e548a944cdc3a84f6cc5c62eee1',1,'Adafruit_Fingerprint_Packet']]]
 ];
