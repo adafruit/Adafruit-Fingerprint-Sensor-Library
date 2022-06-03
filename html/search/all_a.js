466ea951ea885438376c34ef272715e87c2a03c3
Doxygen CI <ci-arduino@invalid>
2022-06-03 15:29:33 +0000

    Deploy docs to GitHub Pages from commit 98df1bbdfc
    Commit: 98df1bbdfc6c3a79469b799fa050d31eaba877cb
    GitHub Actions run: 2435553507

--- html/search/all_a.js
+++ html/search/all_a.js
@@ -2,7 +2,10 @@ var searchData=
 [
   ['security_5flevel',['security_level',['../class_adafruit___fingerprint.html#a19824868b8f1dedbf77f6b9dc1f591b0',1,'Adafruit_Fingerprint']]],
   ['send_5fcmd_5fpacket',['SEND_CMD_PACKET',['../_adafruit___fingerprint_8cpp.html#a8906d9aecdbb4d82f31b7696e58bcc40',1,'Adafruit_Fingerprint.cpp']]],
+  ['setbaudrate',['setBaudRate',['../class_adafruit___fingerprint.html#a5526707615592d1ae2900c26aabb0d02',1,'Adafruit_Fingerprint']]],
+  ['setpacketsize',['setPacketSize',['../class_adafruit___fingerprint.html#a894daa96c2a9b66c21940a025ce13cba',1,'Adafruit_Fingerprint']]],
   ['setpassword',['setPassword',['../class_adafruit___fingerprint.html#a71d38fce7ab2eacb63d222e71cac65af',1,'Adafruit_Fingerprint']]],
+  ['setsecuritylevel',['setSecurityLevel',['../class_adafruit___fingerprint.html#a0c31fe8631f268ce2814584187ad5e44',1,'Adafruit_Fingerprint']]],
   ['start_5fcode',['start_code',['../struct_adafruit___fingerprint___packet.html#af580edb11a176e22cdcac6475a0a754e',1,'Adafruit_Fingerprint_Packet']]],
   ['status_5freg',['status_reg',['../class_adafruit___fingerprint.html#a11c9edd2df20ca6e9a20ece75c116b3b',1,'Adafruit_Fingerprint']]],
   ['storemodel',['storeModel',['../class_adafruit___fingerprint.html#a14d8097e5ce038322913b43e026427e5',1,'Adafruit_Fingerprint']]],
--- html/search/functions_9.js
+++ html/search/functions_9.js
@@ -1,5 +1,8 @@
 var searchData=
 [
+  ['setbaudrate',['setBaudRate',['../class_adafruit___fingerprint.html#a5526707615592d1ae2900c26aabb0d02',1,'Adafruit_Fingerprint']]],
+  ['setpacketsize',['setPacketSize',['../class_adafruit___fingerprint.html#a894daa96c2a9b66c21940a025ce13cba',1,'Adafruit_Fingerprint']]],
   ['setpassword',['setPassword',['../class_adafruit___fingerprint.html#a71d38fce7ab2eacb63d222e71cac65af',1,'Adafruit_Fingerprint']]],
+  ['setsecuritylevel',['setSecurityLevel',['../class_adafruit___fingerprint.html#a0c31fe8631f268ce2814584187ad5e44',1,'Adafruit_Fingerprint']]],
   ['storemodel',['storeModel',['../class_adafruit___fingerprint.html#a14d8097e5ce038322913b43e026427e5',1,'Adafruit_Fingerprint']]]
 ];
