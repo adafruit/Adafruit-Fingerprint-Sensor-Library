3b21704794af40eae7850cac283a7f033ffab840
HAALniner21 <ask150079@gmail.com>
2022-09-06 20:35:25 +0600

    Update Adafruit_Fingerprint.cpp
    Fixed issues with 'write_template_to_sensor' function.

--- Adafruit_Fingerprint.cpp
+++ Adafruit_Fingerprint.cpp
@@ -320,7 +320,7 @@ boolean Adafruit_Fingerprint::write_template_to_sensor(uint8_t ref_buf[]) { //ne
   if(downloadModel(0x01) != FINGERPRINT_OK)return false; //check if buffer 1 is ready to be loaded
   int div=ceil(sizeof(ref_buf)/packet_len);
   uint8_t data[packet_len];
-  memset(ref_buf, 0xff, packet_len);
+  memset(data, 0xff, packet_len);
   Adafruit_Fingerprint_Packet t_packet(FINGERPRINT_DATAPACKET,sizeof(data),data); 
   for(int i=0;i<div;i++){
     if(i==(div-1))t_packet.type=FINGERPRINT_ENDDATAPACKET;
