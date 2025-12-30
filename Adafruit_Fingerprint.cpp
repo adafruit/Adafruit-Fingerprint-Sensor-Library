e875934e9e3151dad87cd6e19d0d0f726fb84c10
Thialf <github@ralf-hoereth.de>
2025-12-30 21:17:04 +0100

    improved search calculations

--- Adafruit_Fingerprint.cpp
+++ Adafruit_Fingerprint.cpp
@@ -307,16 +307,9 @@ uint8_t Adafruit_Fingerprint::emptyDatabase(void) {
 uint8_t Adafruit_Fingerprint::fingerFastSearch(void) {
   // high speed search of slot #1 starting at page 0x0000 and page #0x00A3
   GET_CMD_PACKET(FINGERPRINT_HISPEEDSEARCH, 0x01, 0x00, 0x00, 0x00, 0xA3);
-  fingerID = 0xFFFF;
-  confidence = 0xFFFF;
 
-  fingerID = packet.data[1];
-  fingerID <<= 8;
-  fingerID |= packet.data[2];
-
-  confidence = packet.data[3];
-  confidence <<= 8;
-  confidence |= packet.data[4];
+  fingerID = (uint16_t)packet.data[1] << 8 | packet.data[2];
+  confidence = (uint16_t)packet.data[3] << 8 | packet.data[4];
 
   return packet.data[0];
 }
@@ -370,16 +363,8 @@ uint8_t Adafruit_Fingerprint::fingerSearch(uint8_t slot) {
   GET_CMD_PACKET(FINGERPRINT_SEARCH, slot, 0x00, 0x00, (uint8_t)(capacity >> 8),
                  (uint8_t)(capacity & 0xFF));
 
-  fingerID = 0xFFFF;
-  confidence = 0xFFFF;
-
-  fingerID = packet.data[1];
-  fingerID <<= 8;
-  fingerID |= packet.data[2];
-
-  confidence = packet.data[3];
-  confidence <<= 8;
-  confidence |= packet.data[4];
+  fingerID = (uint16_t)packet.data[1] << 8 | packet.data[2];
+  confidence = (uint16_t)packet.data[3] << 8 | packet.data[4];
 
   return packet.data[0];
 }
